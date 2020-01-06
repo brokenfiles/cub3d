a9d799ecdfab487ee25a9db50b935a4bd5f26841
Brokenfiles <louis.nevysup@gmail.com>
2020-01-06 17:21:31 +0100

    adding get_next_line

---
+++ libs/get_next_line/get_next_line_utils.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llaurent <llaurent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/07 10:19:11 by llaurent          #+#    #+#             */
+/*   Updated: 2020/01/06 17:04:33 by llaurent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+int		contains_char(char *s, char c)
+{
+	int	index;
+
+	index = 0;
+	if (!s)
+		return (0);
+	while (s[index])
+	{
+		if (s[index] == c)
+			return (SUCCESS_CODE_GNL);
+		index++;
+	}
+	return (0);
+}
+
+char	*ft_strncat(char *dest, const char *src, size_t nb)
+{
+	unsigned int	len;
+	unsigned int	index;
+
+	index = 0;
+	len = ft_strlen(dest);
+	while (src[index] && index < nb)
+		dest[len++] = src[index++];
+	dest[len] = 0;
+	return (dest);
+}
+
+char	*ft_strnew(size_t n)
+{
+	char	*string;
+	int		index;
+
+	if (!(string = (char *)malloc(n)))
+		return (NULL);
+	index = 0;
+	while (index < n)
+		string[index++] = 0;
+	return (string);
+}
+
+int		free_string_and_return(char **string, int val)
+{
+	if (*string)
+		free(*string);
+	*string = NULL;
+	return (val);
+}
